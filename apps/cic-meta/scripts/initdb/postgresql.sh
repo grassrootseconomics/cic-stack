7ff9b9d6489b950975d3db617ddbfcb8b446ddcf
Blair Vanderlugt <blair.vanderlugt@gmail.com>
2021-02-09 08:24:47 -0800

    datatbase schema provisioning

--- apps/cic-meta/scripts/initdb/postgresql.sh
+++ apps/cic-meta/scripts/initdb/postgresql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname cic_meta <<-EOSQL
-    create table if not exists cic_meta.store (
+psql -v ON_ERROR_STOP=1 --username grassroots --dbname cic_meta <<-EOSQL
+    create table if not exists store (
         id serial primary key not null,
         owner_fingerprint text not null,
         hash char(64) not null unique,
