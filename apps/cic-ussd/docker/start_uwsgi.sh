0bdcb3b619fe44509278ba443ccbed7dcb3bf93d
nolash <dev@holbrook.no>
2021-02-10 19:50:34 +0100

    Correct host facing ports on docker compose

--- apps/cic-ussd/docker/start_uwsgi.sh
+++ apps/cic-ussd/docker/start_uwsgi.sh
@@ -2,4 +2,5 @@
 
 . /root/db.sh
 
-/usr/local/bin/uwsgi --wsgi-file /usr/local/lib/python3.8/site-packages/cic_ussd/runnable/server.py --http :9000 --pyargv "-vv"
+echo "pooooort $SERVER_PORT"
+/usr/local/bin/uwsgi --wsgi-file /usr/local/lib/python3.8/site-packages/cic_ussd/runnable/server.py --http :$SERVER_PORT --pyargv "-vv"
