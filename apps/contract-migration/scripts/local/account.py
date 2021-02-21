42c441c82dcb1ace2151d1fe30ca5895e1b087eb
nolash <dev@holbrook.no>
2021-02-21 13:52:20 +0100

    Add docstrings

--- apps/contract-migration/scripts/local/account.py
+++ apps/contract-migration/scripts/local/account.py
@@ -12,7 +12,17 @@ name = 'account'
 
 
 def do(token_pair, sender, recipient, sender_balance, aux, block_number, tx_index):
+    """Triggers creation and registration of new account through the custodial cic-eth component.
 
+    It expects the following aux parameters to exist:
+    - redis_host_callback: Redis host name exposed to cic-eth, for callback
+    - redis_port_callback: Redis port exposed to cic-eth, for callback
+    - redis_db: Redis db, for callback
+    - redis_channel: Redis channel, for callback
+    - chain_spec: Chain specification for the chain to execute the transfer on
+
+    See local.noop.do for details on parameters and return values.
+    """
     logg.debug('running {} {} {}'.format(__name__, token_pair, sender, recipient))
     api = Api(
         str(aux['chain_spec']),
@@ -24,4 +34,4 @@ def do(token_pair, sender, recipient, sender_balance, aux, block_number, tx_inde
 
     t = api.create_account(register=True)
 
-    return (None, t, sender_balance, sender_balance,)
+    return (None, t, sender_balance, )
--- apps/contract-migration/scripts/local/transfer.py
+++ apps/contract-migration/scripts/local/transfer.py
@@ -13,13 +13,26 @@ name = 'erc20_transfer'
 
 
 def do(token_pair, sender, recipient, sender_balance, aux, block_number, tx_index):
+    """Triggers an ERC20 token transfer through the custodial cic-eth component, with a randomly chosen amount in integer resolution.
 
+    It expects the following aux parameters to exist:
+    - redis_host_callback: Redis host name exposed to cic-eth, for callback
+    - redis_port_callback: Redis port exposed to cic-eth, for callback
+    - redis_db: Redis db, for callback
+    - redis_channel: Redis channel, for callback
+    - chain_spec: Chain specification for the chain to execute the transfer on
+
+    See local.noop.do for details on parameters and return values.
+    """
     logg.debug('running {} {} {} {}'.format(__name__, token_pair, sender, recipient))
 
     decimals = token_pair[0].decimals()
-    balance_units = int(sender_balance / decimals)
 
-    if balance_units == 0:
+    sender_balance_value = sender_balance['balance_network'] - sender_balance['balance_outgoing']
+
+    balance_units = int(sender_balance_value / decimals)
+
+    if balance_units <= 0:
         return (AttributeError('sender {} has zero balance'), None, 0,)
 
     spend_units = random.randint(1, balance_units)
@@ -34,6 +47,5 @@ def do(token_pair, sender, recipient, sender_balance, aux, block_number, tx_inde
         )
     t = api.transfer(sender, recipient, spend_value, token_pair[0].symbol())
 
-    changed_sender_balance = sender_balance - spend_value
-
-    return (None, t, changed_sender_balance,)
+    sender_balance['balance_outgoing'] += spend_value
+    return (None, t, sender_balance,)
