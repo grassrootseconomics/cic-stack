a6fd20124c6ba73c79044a8ef6016af13c6bc783
nolash <dev@holbrook.no>
2021-03-13 21:31:37 +0100

    WIP receive new registry

--- apps/cic-eth/tests/conftest.py
+++ apps/cic-eth/tests/conftest.py
@@ -1,13 +1,14 @@
 # standard imports
 import os
 import sys
+import logging
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.dirname(script_dir)
 sys.path.insert(0, root_dir)
 
 from tests.fixtures_config import *
-from tests.fixtures_chainlib import *
 from tests.fixtures_celery import *
 from tests.fixtures_database import *
 from tests.fixtures_registry import *
+from chainlib.pytest.fixtures_ethtester import *
--- apps/cic-eth/tests/fixtures_registry.py
+++ apps/cic-eth/tests/fixtures_registry.py
@@ -1,7 +1,29 @@
 # external imports
 import pytest
+from cic_registry import CICRegistry
 from chainlib.chain import ChainSpec
+from eth_accounts_index import AccountRegistry
 
 @pytest.fixture(scope='session')
 def default_chain_spec():
     return ChainSpec('evm', 'bloxberg', 8996)
+
+
+@pytest.fixture(scope='session')
+def accounts_registry(
+        default_chain_spec,
+        cic_registry,
+        ):
+    #abi = AccountRegistry.abi()
+    #constructor = w3.eth.contract(abi=abi, bytecode=AccountRegistry.bytecode())
+    #tx_hash = constructor.constructor().transact()
+    r = w3.eth.getTransactionReceipt(tx_hash) 
+    logg.debug('accounts registry deployed {}'.format(r.contractAddress))
+    account_registry = AccountRegistry(w3, r.contractAddress)
+
+    c = w3.eth.contract(abi=abi, address=r.contractAddress)
+    c.functions.addWriter(w3.eth.accounts[0]).transact()
+
+    CICRegistry.add_contract(default_chain_spec, c, 'AccountRegistry')
+
+    return account_registry
