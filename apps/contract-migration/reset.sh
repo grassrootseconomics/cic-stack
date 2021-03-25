5bf30afb0755f665cb6ac65c68bcd9cc0c761524
nolash <dev@holbrook.no>
2021-03-25 09:34:57 +0100

    WIP Rehabilitate seed scrip

--- apps/contract-migration/reset.sh
+++ apps/contract-migration/reset.sh
@@ -30,6 +30,8 @@ echo -n 1 > $init_level_file
 # Abort on any error (including if wait-for-it fails).
 set -e
 
+echo wtf
+
 # Wait for the backend to be up, if we know where it is.
 if [[ -n "${ETH_PROVIDER}" ]]; then
 	echo "waiting for ${ETH_PROVIDER}..."
@@ -42,7 +44,7 @@ if [[ -n "${ETH_PROVIDER}" ]]; then
 
 	DEV_ACCOUNT_INDEX_ADDRESS=`eth-accounts-index-deploy -i $CIC_CHAIN_SPEC -p $ETH_PROVIDER -y $keystore_file -vv -w`
 
-	CIC_REGISTRY_ADDRESS=`eth-contract-registry-deploy -i $CIC_CHAIN_SPEC -y $keystore_file --identifier CICRegistry --identifier BancorRegistry --identifier AccountRegistry --identifier TokenRegistry --identifier AddressDeclarator --identifier Faucet --identifier TransferAuthorization -p $ETH_PROVIDER -vv -w`
+	CIC_REGISTRY_ADDRESS=`eth-contract-registry-deploy -i $CIC_CHAIN_SPEC -y $keystore_file --identifier BancorRegistry --identifier AccountRegistry --identifier TokenRegistry --identifier AddressDeclarator --identifier Faucet --identifier TransferAuthorization -p $ETH_PROVIDER -vv -w`
 	eth-contract-registry-set -w -y $keystore_file -r $CIC_REGISTRY_ADDRESS -i $CIC_CHAIN_SPEC  -p $ETH_PROVIDER -vv ContractRegistry $CIC_REGISTRY_ADDRESS
 	#cic-registry-set -r $CIC_REGISTRY_ADDRESS -i $CIC_CHAIN_SPEC -k BancorRegistry -p $ETH_PROVIDER $BANCOR_REGISTRY_ADDRESS -vv
 	eth-contract-registry-set -w -y $keystore_file -r $CIC_REGISTRY_ADDRESS -i $CIC_CHAIN_SPEC -p $ETH_PROVIDER -vv AccountRegistry $DEV_ACCOUNT_INDEX_ADDRESS
@@ -62,6 +64,8 @@ if [[ -n "${ETH_PROVIDER}" ]]; then
 	>&2 echo "deploy token index contract"
 	DEV_TOKEN_INDEX_ADDRESS=`eth-token-index-deploy -y $keystore_file -i $CIC_CHAIN_SPEC -p $ETH_PROVIDER -w -v`
 	eth-contract-registry-set -w -y $keystore_file -r $CIC_REGISTRY_ADDRESS -i $CIC_CHAIN_SPEC -p $ETH_PROVIDER -vv TokenRegistry $DEV_TOKEN_INDEX_ADDRESS 
+	>&2 echo "add reserve token to token index"
+	eth-token-index-add -w -y $keystore_file  -i $CIC_CHAIN_SPEC -p $ETH_PROVIDER -vv -a $DEV_TOKEN_INDEX_ADDRESS $DEV_RESERVE_ADDRESS
 
 	# Sarafu faucet contract
 	>&2 echo "deploy token faucet contract"
--- apps/contract-migration/run_job.sh
+++ apps/contract-migration/run_job.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-if [[ $RUN_LEVEL -eq 1 ]]
+if [[ $((RUN_MASK & 1)) -eq 1 ]]
 then
   ./reset.sh
 fi
 
-if [[ $RUN_LEVEL -eq 2 ]]
+if [[ $((RUN_MASK & 2)) -eq 2 ]]
 then
   ./seed_cic_eth.sh
 fi
