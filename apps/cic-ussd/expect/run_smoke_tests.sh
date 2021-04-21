6dd775051c6d405ba8bef351529a8897e2bb84c0
PhilipWafula <philipwafula2@gmail.com>
2021-04-21 13:45:04 +0300

    Refactors to improve expect scripts.

--- apps/cic-ussd/expect/run_smoke_tests.sh
+++ apps/cic-ussd/expect/run_smoke_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pyresttest "$TEST_SERVER_URL" test_suite.yml
+if [ -z "$TEST_SERVER_URL" ];
+then
+    echo "The test server url is not set !"
+    exit 1
+fi
+
+pyresttest "$TEST_SERVER_URL" ./test_suite.yml --log debug
