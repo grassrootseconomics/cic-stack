ea3c75e7556ed8d5cb95e35f4d7dab22b53bbe8b
nolash <dev@holbrook.no>
2021-10-17 14:30:42 +0200

    Rehabilitate traffic script

--- apps/contract-migration/3_deploy_token.sh
+++ apps/contract-migration/3_deploy_token.sh
@@ -59,12 +59,23 @@ function deploy_token_erc20_demurrage_token() {
 	TOKEN_ADDRESS=`erc20-demurrage-token-deploy $fee_price_arg -p $RPC_PROVIDER -y $WALLET_KEY_FILE -i $CHAIN_SPEC --name "$TOKEN_NAME" --symbol $TOKEN_SYMBOL $DEV_DEBUG_FLAG -ww -s` 
 }
 
+function deploy_accounts_index() {
+	# Deploy accounts index contact
+	>&2 echo -e "\033[;96mDeploy accounts index contract for token $TOKEN_SYMBOL\033[;39m"
+	DEV_ACCOUNTS_INDEX_ADDRESS=`okota-accounts-index-deploy $gas_price_arg -u -s -w -y $WALLET_KEY_FILE -i $CHAIN_SPEC -p $RPC_PROVIDER $DEV_DEBUG_FLAG --address-declarator $DEV_ADDRESS_DECLARATOR --token-address $1`
+
+	if [ -z "$have_default_token" ]; then
+		>&2 echo -e "\033[;96mAdd acccounts index record for default token to contract registry\033[;39m"
+		r=`eth-contract-registry-set $fee_price_arg -s -u -w -y $WALLET_KEY_FILE -e $CIC_REGISTRY_ADDRESS -i $CHAIN_SPEC  -p $RPC_PROVIDER $DEV_DEBUG_FLAG --identifier AccountRegistry $DEV_ACCOUNTS_INDEX_ADDRESS`
+		add_pending_tx_hash $r
+	fi
+}
 
 function deploy_minter_faucet() {
 	FAUCET_AMOUNT=${FAUCET_AMOUNT:-0}
 
 	# Token faucet contract
-	>&2 echo -e "\033[;96mDeploy token faucet contract\033[;39m"
+	>&2 echo -e "\033[;96mDeploy token faucet contract for token $TOKEN_SYMBOL\033[;39m"
 	accounts_index_address=`eth-contract-registry-list -u -i $CHAIN_SPEC -p $RPC_PROVIDER -e $CIC_REGISTRY_ADDRESS $DEV_DEBUG_FLAG --raw AccountRegistry`
 	faucet_address=`sarafu-faucet-deploy $fee_price_arg -s -y $WALLET_KEY_FILE -i $CHAIN_SPEC -p $RPC_PROVIDER -w $DEV_DEBUG_FLAG --account-index-address $accounts_index_address $1`
 
@@ -77,7 +88,6 @@ function deploy_minter_faucet() {
 		r=`eth-contract-registry-set -s -u $fee_price_arg -w -y $WALLET_KEY_FILE -e $CIC_REGISTRY_ADDRESS -i $CHAIN_SPEC -p $RPC_PROVIDER $DEV_DEBUG_FLAG --identifier Faucet $faucet_address`
 		add_pending_tx_hash $r
 	fi
-
 
 	>&2 echo -e "\033[;96mSet faucet as token minter\033[;39m"
 	r=`giftable-token-minter -s -u $fee_price_arg -w -y $WALLET_KEY_FILE -e $TOKEN_ADDRESS -i $CHAIN_SPEC -p $RPC_PROVIDER $DEV_DEBUG_FLAG $faucet_address`
@@ -95,16 +105,20 @@ if [ -z "$have_default_token" ]; then
 fi
 
 
->&2 echo -e "\033[;96mAdd token to token index\033[;39m"
+>&2 echo -e "\033[;96mAdd token symbol $TOKEN_SYMBOL to token address $TOKEN_ADDRESS mapping to token index\033[;39m"
 token_index_address=`eth-contract-registry-list -u -i $CHAIN_SPEC -p $RPC_PROVIDER -e $CIC_REGISTRY_ADDRESS $DEV_DEBUG_FLAG --raw TokenRegistry`
 r=`eth-token-index-add $fee_price_arg -s -u -w -y $WALLET_KEY_FILE  -i $CHAIN_SPEC -p $RPC_PROVIDER $DEV_DEBUG_FLAG -e $token_index_address $TOKEN_ADDRESS`
 add_pending_tx_hash $r
 
 
->&2 echo -e "\033[;96mMinting $DEV_TOKEN_MINT_AMOUNT tokens\033[;39m"
+TOKEN_MINT_AMOUNT=${TOKEN_MINT_AMOUNT:-${DEV_TOKEN_MINT_AMOUNT}}
+>&2 echo -e "\033[;96mMinting $TOKEN_MINT_AMOUNT tokens\033[;39m"
 r=`giftable-token-gift $fee_price_arg -p $RPC_PROVIDER -y $WALLET_KEY_FILE -i $CHAIN_SPEC -u $DEV_DEBUG_FLAG -s -w -e $TOKEN_ADDRESS "$DEV_TOKEN_MINT_AMOUNT"`
 add_pending_tx_hash $r
 
+
+# Create accounts index for default token
+deploy_accounts_index $TOKEN_ADDRESS
 
 # Connect a minter component if defined
 TOKEN_MINTER_MODE=${TOKEN_MINTER_MODE:-"faucet"}
